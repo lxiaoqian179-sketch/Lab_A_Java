fd67a1f6cc903cb9d27d95507acda91725122526
lxiaoqian179-sketch <lxiaoqian179@gmail.com>
2026-01-05 16:00:48 +0800

    bicycle

---
+++ src/lab_A/Brad_17.java
@@ -0,0 +1,20 @@
+package lab_A;
+
+public class Brad_17 {
+//	腳踏車speed
+	
+	
+	
+	public static void main(String[] args) {
+	}
+
+	public double speed;
+
+	public void upSpeed(double delta) {
+		speed += delta;
+	}
+
+	public void downSpeed(double delta) {
+		speed -= delta;
+	}
+}
